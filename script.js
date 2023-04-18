9a57ce74e381e8fcc7e9e8102904070ecda0578f
Kewin Pyza <kewin.pyza@gmail.com>
2023-04-18 10:20:34 +0200

    Add point 3

--- script.js
+++ script.js
@@ -7,15 +7,15 @@
 // Retrieve user data
 const getUserData = async () => {
   const response = await fetch("https://fakestoreapi.com/users");
-  const users = await response.json();
-  return users;
+  const data = await response.json();
+  return data;
 };
 
 // Retrieve product data
 const getProductData = async () => {
   const response = await fetch("https://fakestoreapi.com/products");
-  const products = await response.json();
-  return products;
+  const data = await response.json();
+  return data;
 };
 
 // Retrieve shopping cart data
@@ -23,14 +23,14 @@ const getCartData = async () => {
   const response = await fetch(
     "https://fakestoreapi.com/carts/?startdate=2000-01-01&enddate=2023-04-07"
   );
-  const carts = await response.json();
-  return carts;
+  const data = await response.json();
+  return data;
 };
 
 // Show data from promises
-getUserData().then((data) => console.log("Users:", data));
-getProductData().then((data) => console.log("Products:", data));
-getCartData().then((data) => console.log("Shopping carts:", data));
+getUserData().then((data) => console.log("Point 1, Users:", data));
+getProductData().then((data) => console.log("Point 1, Products:", data));
+getCartData().then((data) => console.log("Point 1, Shopping carts:", data));
 
 // 2. Creates a data structure containing all available product categories and the total value of
 // products of a given category
@@ -39,15 +39,50 @@ const getCategories = async () => {
   const products = await getProductData();
   const categories = {};
 
-  products.forEach((product) => {
-    if (product.category in categories) {
-      categories[product.category] += product.price;
-    } else {
+  for (const product of products) {
+    if (!categories[product.category]) {
       categories[product.category] = product.price;
+    } else {
+      categories[product.category] += product.price;
     }
-  });
+  }
 
   return categories;
 };
 
-getCategories().then((obj) => console.log(obj));
+getCategories().then((obj) => console.log("Point 2:", obj));
+
+// 3. Finds a cart with the highest value, determines its value and full name of its owner
+
+async function getHighestCartValue() {
+  const users = await getUserData();
+  const carts = await getCartData();
+  const products = await getProductData();
+
+  const cartValues = carts.map((cart) => {
+    const owner = users.find((user) => user.id === cart.userId);
+    const fullName = `${
+      owner.name.firstname[0].toUpperCase() + owner.name.firstname.slice(1)
+    } ${owner.name.lastname[0].toUpperCase() + owner.name.lastname.slice(1)}`;
+    const cartValue = cart.products.reduce((totalValue, cartProduct) => {
+      const product = products.find((p) => p.id === cartProduct.productId);
+      return totalValue + product.price * cartProduct.quantity;
+    }, 0);
+    return { id: cart.id, owner: fullName, value: cartValue };
+  });
+
+  const highestValueCart = cartValues.reduce((a, b) =>
+    a.value > b.value ? a : b
+  );
+  const ownerWithHighestValue = highestValueCart.owner;
+  const highestValue = highestValueCart.value;
+  const highestCartId = highestValueCart.id;
+
+  return {
+    cartId: highestCartId,
+    owner: ownerWithHighestValue,
+    value: highestValue,
+  };
+}
+
+getHighestCartValue().then((data) => console.log("Point 3:", data));
